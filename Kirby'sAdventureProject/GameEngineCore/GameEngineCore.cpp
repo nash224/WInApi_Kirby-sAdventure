f436e72298a6ee8dcdbddd8587c945553dfa6ae1
김태훈 <hyri0510@gmail.com>
2023-05-24 17:36:46 +0900

    김태훈 : [커비의모험] 05-24 최종 동기화 업데이트

--- Kirby'sAdventureProject/GameEngineCore/GameEngineCore.cpp
+++ Kirby'sAdventureProject/GameEngineCore/GameEngineCore.cpp
@@ -1,16 +1,16 @@
 #include "GameEngineCore.h"
-#include "GameEngineLevel.h"
-
-#include <GameEngineBase/GameEngineTime.h>
 #include <GameEnginePlatform/GameEngineWindow.h>
+#include <GameEngineBase/GameEngineDebug.h>
+#include <GameEngineBase/GameEngineTime.h>
+#include "GameEngineLevel.h"
 #include <GameEnginePlatform/GameEngineInput.h>
-#include "GameEngineCamera.h"
+#include <GameEnginePlatform/GameEngineSound.h>
 
 std::string GameEngineCore::WindowTitle = "";
 std::map<std::string, class GameEngineLevel*> GameEngineCore::AllLevel;
 CoreProcess* GameEngineCore::Process = nullptr;
-GameEngineLevel* GameEngineCore::NextLevel = nullptr;
 GameEngineLevel* GameEngineCore::CurLevel = nullptr;
+GameEngineLevel* GameEngineCore::NextLevel = nullptr;
 
 GameEngineCore::GameEngineCore()
 {
@@ -20,26 +20,13 @@ GameEngineCore::~GameEngineCore()
 {
 }
 
-
-
-// C2511 : 선언을 안해서 오버로드된 멤버 함수를 찾음
-void GameEngineCore::EngineStart(const std::string& _Title, HINSTANCE _Inst, CoreProcess* _Ptr)
-{
-	GameEngineDebug::LeckCheck();
-
-	Process = _Ptr;
-	WindowTitle = _Title;
-
-	GameEngineWindow::MessageLoop(_Inst, CoreStart, CoreUpdate, CoreEnd);
-}
-
-
-
 void GameEngineCore::CoreStart(HINSTANCE _Inst)
 {
-    GameEngineWindow::MainWindow.Open(WindowTitle, _Inst);
+	// 엔진쪽에 준비를 다 해고
+	GameEngineWindow::MainWindow.Open(WindowTitle, _Inst);
 	GameEngineInput::InputInit();
 
+	// 유저의 준비를 해준다.
 	Process->Start();
 }
 
@@ -54,13 +41,15 @@ void GameEngineCore::CoreUpdate()
 		}
 
 		NextLevel->ActorLevelStart();
-		NextLevel->LevelStart(NextLevel);
+		NextLevel->LevelStart(CurLevel);
 
 		CurLevel = NextLevel;
+
 		NextLevel = nullptr;
 		GameEngineTime::MainTimer.Reset();
 	}
 
+	// 업데이트를 
 	GameEngineTime::MainTimer.Update();
 	float Delta = GameEngineTime::MainTimer.GetDeltaTime();
 
@@ -73,15 +62,22 @@ void GameEngineCore::CoreUpdate()
 		GameEngineInput::Reset();
 	}
 
+	// 한프레임 동안은 절대로 기본적인 세팅의 
+	// 변화가 없게 하려고 하는 설계의도가 있는것.
+	// 이걸 호출한 애는 PlayLevel
 	CurLevel->AddLiveTime(Delta);
 	CurLevel->Update(Delta);
+
+	// TitleLevel
 	CurLevel->ActorUpdate(Delta);
 	GameEngineWindow::MainWindow.ClearBackBuffer();
 	CurLevel->ActorRender(Delta);
 	CurLevel->Render(Delta);
 	GameEngineWindow::MainWindow.DoubleBuffering();
 
+	// 프레임의 가장 마지막에 Release가 될겁니다.
 	CurLevel->ActorRelease();
+
 }
 
 void GameEngineCore::CoreEnd()
@@ -94,14 +90,24 @@ void GameEngineCore::CoreEnd()
 		Process = nullptr;
 	}
 
-	for (std::pair<std::string, GameEngineLevel*> _Ptr : AllLevel)
+	for (std::pair<std::string, GameEngineLevel*> _Pair : AllLevel)
 	{
-		if (nullptr != _Ptr.second)
+		if (nullptr != _Pair.second)
 		{
-			delete _Ptr.second;
-			_Ptr.second = nullptr;
+			delete _Pair.second;
+			_Pair.second = nullptr;
 		}
 	}
+}
+
+
+void GameEngineCore::EngineStart(const std::string& _Title, HINSTANCE _Inst, CoreProcess* _Ptr)
+{
+	GameEngineDebug::LeckCheck();
+
+	Process = _Ptr;
+	WindowTitle = _Title;
+	GameEngineWindow::MessageLoop(_Inst, CoreStart, CoreUpdate, CoreEnd);
 }
 
 void GameEngineCore::LevelInit(GameEngineLevel* _Level)
