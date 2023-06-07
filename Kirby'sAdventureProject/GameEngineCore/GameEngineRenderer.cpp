abaa1d07a1ec7ab8800536ca6354a18cb2efee47
김태훈 <hyri0510@gmail.com>
2023-06-07 22:11:23 +0900

    커밋

--- Kirby'sAdventureProject/GameEngineCore/GameEngineRenderer.cpp
+++ Kirby'sAdventureProject/GameEngineCore/GameEngineRenderer.cpp
@@ -262,6 +262,52 @@ void GameEngineRenderer::CreateAnimation(
 
 }
 
+void GameEngineRenderer::CreateAnimationToFrame(
+	const std::string& _AniamtionName,
+	const std::string& _SpriteName,
+	const std::vector<size_t>& _Frame,
+	float _Inter,
+	bool _Loop)
+{
+	std::string UpperName = GameEngineString::ToUpperReturn(_AniamtionName);
+
+	if (nullptr != FindAnimation(UpperName))
+	{
+		MsgBoxAssert("이미 존재하는 애니메이션 네임입니다." + UpperName);
+		return;
+	}
+
+	GameEngineSprite* Sprite = ResourcesManager::GetInst().FindSprite(_SpriteName);
+
+	if (nullptr == Sprite)
+	{
+		MsgBoxAssert("존재하지 않는 스프라이트로 애니메이션을 만들려고 했습니다." + _SpriteName);
+		return;
+	}
+
+	GameEngineRenderer::Animation& Animation = AllAnimation[UpperName];
+
+	Animation.Sprite = Sprite;
+	Animation.StartFrame = _Frame[0];
+	Animation.EndFrame = _Frame[_Frame.size() - 1];
+
+	// 0 - 5 - 5
+	// 역
+
+	// 0, 0
+	Animation.Inters.resize(_Frame.size());
+	Animation.Frames.resize(_Frame.size());
+
+	Animation.Frames = _Frame;
+
+	for (size_t i = 0; i < Animation.Frames.size(); i++)
+	{
+		Animation.Inters[i] = _Inter;
+	}
+
+	Animation.Loop = _Loop;
+}
+
 
 void GameEngineRenderer::ChangeAnimation(const std::string& _AniamtionName, int _FrameCount, bool _ForceChange)
 {
